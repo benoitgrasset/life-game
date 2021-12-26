0a4e70ab2f61d239124fc5cd0b408f6dff7f9991
benoitgrasset <benoit.grasset@gmail.com>
2021-12-26 22:00:16 +0100

    update parameters

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-const col = 5;
-const row = 5;
-
 const buttonStyle = {
   marginBottom: '10px',
   marginRight: '5px',
@@ -10,30 +7,7 @@ const buttonStyle = {
   height: '30px',
 };
 
-const getRandomBoolean = () => Math.random() < 0.5;
-
-const getArray = () => {
-  let array = Array.from(Array(row), () => new Array(col).fill(false));
-  for (let i = 1; i < row - 1; i++) {
-    for (let j = 1; j < col - 1; j++) {
-      array[i][j] = getRandomBoolean();
-    }
-  }
-  return array;
-};
-
-const getNewArray = (array: boolean[][]) => {
-  let newArray = array;
-  for (let i = 1; i < row - 1; i++) {
-    for (let j = 1; j < col - 1; j++) {
-      const reducedArray = getReducedArray(newArray, i, j);
-      if (array[i][j] === true) {
-        newArray[i][j] = has2or3Neighbors(reducedArray);
-      } else newArray[i][j] = has3Neighbors(reducedArray);
-    }
-  }
-  return newArray;
-};
+const getRandomBoolean = () => Math.random() < 0.15;
 
 const getReducedArray = (array: boolean[][], i: number, j: number) => {
   const reducedArray = [
@@ -54,12 +28,44 @@ const nbOfTrue = (array: boolean[][]) => {
 };
 
 export default function App() {
-  const [array, setArray] = React.useState(getArray());
   const [lap, setLap] = React.useState(0);
+  const [col, setCol] = React.useState(50);
+  const [row, setRow] = React.useState(50);
+
+  const getArray = () => {
+    let array = Array.from(Array(row), () => new Array(col).fill(false));
+    for (let i = 1; i < row - 1; i++) {
+      for (let j = 1; j < col - 1; j++) {
+        array[i][j] = getRandomBoolean();
+      }
+    }
+    return array;
+  };
+
+  const [array, setArray] = React.useState(getArray());
+
+  const getNewArray = (array: boolean[][]) => {
+    let newArray = array;
+    for (let i = 1; i < row - 1; i++) {
+      for (let j = 1; j < col - 1; j++) {
+        const reducedArray = getReducedArray(newArray, i, j);
+        if (array[i][j] === true) {
+          newArray[i][j] = has2or3Neighbors(reducedArray);
+        } else newArray[i][j] = has3Neighbors(reducedArray);
+      }
+    }
+    return newArray;
+  };
 
   const handleNext = () => {
     const newArray = getNewArray(array);
     setArray(newArray);
+  };
+  const handleRowChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRow(parseInt(event.target.value));
+  };
+  const handleColChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCol(parseInt(event.target.value));
   };
 
   const handleGenerate = () => {
@@ -71,18 +77,20 @@ export default function App() {
     const interval = setInterval(() => {
       handleNext();
       setLap(lap => lap + 1);
-    }, 2000);
+    }, 1500);
     return () => clearInterval(interval);
   }, [handleNext]);
 
   return (
-    <div>
+    <>
       <button onClick={handleNext} style={buttonStyle}>
         Next
       </button>
       <button onClick={handleGenerate} style={buttonStyle}>
         Generate
       </button>
+      row: <input type="text" id="row" name="row" value={row} onChange={handleRowChange} style={{ marginRight: '5px' }} />
+      col: <input type="text" id="col" name="col" value={col} onChange={handleColChange} style={{ marginRight: '5px' }} />
       lap: {lap}
       <div style={{ display: 'flex', flexDirection: 'column' }}>
         {array.map((row) => {
@@ -96,8 +104,8 @@ export default function App() {
                   <div
                     style={{
                       padding: '3px',
-                      width: '30px',
-                      height: '30px',
+                      width: '25px',
+                      height: '25px',
                       textAlign: 'center',
                       border: '1px solid black',
                       ...style,
@@ -109,6 +117,6 @@ export default function App() {
           );
         })}
       </div>
-    </div>
+    </>
   );
 }
